24af169066522671bde7527488931f365f63aee5
Nick Wells <wells.nicholas@gmail.com>
2026-02-02 21:54:38 +0000

    refactor: JoinQuoted now has default quotes
    The quotes parameters are now a variadic slice rather than a pair of explicit
    values. If the slice is empty then the opening and closing quotes default to
    '"'. If one value is given then the opening and closing quotes are set to the
    same value. If two values are given then the opening quote is the first and
    the closing is the second. More than two values are silently ignored.

--- english/join.go
+++ english/join.go
@@ -24,10 +24,25 @@ func Join(s []string, sep, finalSep string) string {
 // JoinQuoted joins the strings in s using the sep for all but the final join
 // for which it uses finalSep. If s is empty it will return the empty string,
 // if it has only one entry it wil return just that entry. Each string in s
-// is quoted with the supplied opening and closing quotes. This allows you to
-// produce joined strings like: "'a', 'b', 'c' or 'd'".
-func JoinQuoted(s []string, sep, finalSep, openQ, closeQ string) string {
+// is quoted with the supplied opening and closing quotes. If no quotes are
+// given both quotes default to '"'. If only one value is given then both the
+// opening and closing quotes are set to this value. If more than one value
+// is given the opening quote is set to the first value and the closing quote
+// is set to the second; subsequent values are silently ignored. This allows
+// you to produce joined strings like: "'a', 'b', 'c' or 'd'".
+func JoinQuoted(s []string, sep, finalSep string, quotes ...string) string {
 	joined := ""
+	openQ := `"`
+	closeQ := `"`
+
+	if len(quotes) > 0 {
+		openQ = quotes[0]
+		closeQ = quotes[0]
+
+		if len(quotes) > 1 {
+			closeQ = quotes[1]
+		}
+	}
 
 	switch len(s) {
 	case 0:
--- english/join_test.go
+++ english/join_test.go
@@ -47,31 +47,82 @@ func TestJoin(t *testing.T) {
 func TestJoinQuoted(t *testing.T) {
 	testCases := []struct {
 		testhelper.ID
-		s   []string
-		exp string
+		s      []string
+		quotes []string
+		exp    string
 	}{
 		{
-			ID: testhelper.MkID("empty slice"),
+			ID:     testhelper.MkID("empty slice. Two, differing quotes"),
+			quotes: []string{"\u00ab", "\u00bb"},
 		},
 		{
-			ID:  testhelper.MkID("one entry"),
+			ID:     testhelper.MkID("one entry. Two, differing quotes"),
+			s:      []string{"one"},
+			quotes: []string{"\u00ab", "\u00bb"},
+			exp:    "«one»",
+		},
+		{
+			ID:     testhelper.MkID("two entries. Two, differing quotes"),
+			s:      []string{"one", "two"},
+			quotes: []string{"\u00ab", "\u00bb"},
+			exp:    "«one» or «two»",
+		},
+		{
+			ID:     testhelper.MkID("three entries. Two, differing quotes"),
+			s:      []string{"one", "two", "three"},
+			quotes: []string{"\u00ab", "\u00bb"},
+			exp:    "«one», «two» or «three»",
+		},
+		{
+			ID:     testhelper.MkID("three entries. Three, differing quotes"),
+			s:      []string{"one", "two", "three"},
+			quotes: []string{"\u00ab", "\u00bb", "<>"},
+			exp:    "«one», «two» or «three»",
+		},
+		{
+			ID:     testhelper.MkID("empty slice. One quote value"),
+			quotes: []string{"'"},
+		},
+		{
+			ID:     testhelper.MkID("one entry. One quote value"),
+			s:      []string{"one"},
+			quotes: []string{"'"},
+			exp:    "'one'",
+		},
+		{
+			ID:     testhelper.MkID("two entries. One quote value"),
+			s:      []string{"one", "two"},
+			quotes: []string{"'"},
+			exp:    "'one' or 'two'",
+		},
+		{
+			ID:     testhelper.MkID("three entries. One quote value"),
+			s:      []string{"one", "two", "three"},
+			quotes: []string{"'"},
+			exp:    "'one', 'two' or 'three'",
+		},
+		{
+			ID: testhelper.MkID("empty slice. No quote values"),
+		},
+		{
+			ID:  testhelper.MkID("one entry. No quote values"),
 			s:   []string{"one"},
-			exp: "«one»",
+			exp: `"one"`,
 		},
 		{
-			ID:  testhelper.MkID("two entries"),
+			ID:  testhelper.MkID("two entries. No quote values"),
 			s:   []string{"one", "two"},
-			exp: "«one» or «two»",
+			exp: `"one" or "two"`,
 		},
 		{
-			ID:  testhelper.MkID("three entries"),
+			ID:  testhelper.MkID("three entries. No quote values"),
 			s:   []string{"one", "two", "three"},
-			exp: "«one», «two» or «three»",
+			exp: `"one", "two" or "three"`,
 		},
 	}
 
 	for _, tc := range testCases {
-		result := english.JoinQuoted(tc.s, ", ", " or ", "\u00ab", "\u00bb")
+		result := english.JoinQuoted(tc.s, ", ", " or ", tc.quotes...)
 		if result != tc.exp {
 			t.Log(tc.IDStr())
 			t.Log("\t: expected: ", tc.exp)
